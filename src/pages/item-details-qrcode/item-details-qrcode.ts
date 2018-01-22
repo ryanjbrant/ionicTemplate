b952d14ee3389b034528d5e335cf04f2277204aa
Ryan Brant <ryanjbrant@gmail.com>
2018-01-22 14:07:27 -0800

    Commit 2
    asd

---
+++ src/pages/item-details-qrcode/item-details-qrcode.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+import { IService } from '../../services/IService';
+
+@IonicPage()
+@Component({
+  templateUrl: 'item-details-qrcode.html'
+})
+export class ItemDetailsPageQRCode {
+
+  page: any;
+  service: IService;
+  params: any = {};
+
+  constructor(public navCtrl: NavController, navParams: NavParams, private barcodeScanner: BarcodeScanner) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.page = navParams.get('page');
+    this.service = navParams.get('service');
+    if (this.service) {
+      this.params = this.service.prepareParams(this.page, navCtrl);
+      this.scanner();
+    } else {
+      navCtrl.setRoot("HomePage");
+    }
+
+  }
+
+  scanner() {
+    this.barcodeScanner.scan()
+      .then((result) => {
+        this.params.data = result;
+      })
+      .catch((error) => {
+        alert(error);
+      })
+  }
+}
---
+++ src/pages/tab-page-6/tab-page-6.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+import { ToastService } from '../../services/toast-service';
+import { TabsService } from '../../services/tabs-service';
+
+@IonicPage()
+@Component({
+  templateUrl: 'tab-page-6.html',
+  providers: [TabsService]
+})
+export class TabPage6 {
+  params: any;
+
+  constructor(private tabsService: TabsService, private toastCtrl: ToastService) {
+    this.tabsService.load("tab6").subscribe(snapshot => {
+      this.params = snapshot;
+    });
+  }
+
+  ngOnChanges(changes: { [propKey: string]: any }) {
+    this.params = changes['data'].currentValue;
+  }
+
+  onItemClick(item:any, e:any) {
+    if (e) {
+      e.stopPropagation();
+    }
+    this.toastCtrl.presentToast(item.title);
+  }
+
+  onShare(item:any, e:any) {
+    if (e) {
+      e.stopPropagation();
+    }
+    this.toastCtrl.presentToast("Share");
+  }
+}
